0b42c8cdb391b5ec090e4789b653288fc72efdb1
gautam <gautambabariya0125@gmail.com>
2024-05-21 12:01:52 +0530

    g

--- index.js
+++ index.js
@@ -165,8 +165,7 @@ app.post('/api/checkauth', async (req, res) => {
 app.post('/products',async (req, res) => {
     try {
         // const file0 = req.files.file;
-        const file0 = req.files;
-        console.log(file0);
+        // const file0 = req.files;
     //     var imageFilename;
     //    await cloudinary.uploader.upload(file0.tempFilePath, (err,result)=>{
             
